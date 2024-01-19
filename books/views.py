bdc26aad5c4e7c906026e80faf5630820934e05d
Ana Freitas <ana.freitaas16@gmail.com>
2024-01-18 21:48:38 -0300

    view: created list books view

--- books/views.py
+++ books/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-
-
-def home(request):
-    return render(request, "books/home.html")
+def books_list(request):
+    return render(request, "books/books_list.html")
--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from books.views import home
+from books.views import books_list
 
-urlpatterns = [path("admin/", admin.site.urls), path("", home)]
+urlpatterns = [
+    path("admin/", admin.site.urls), 
+    path("", books_list),
+]
