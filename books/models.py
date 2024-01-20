b569616bdcf9f55775d14543b9cca95a39413322
Ana Freitas <ana.freitaas16@gmail.com>
2024-01-19 23:21:47 -0300

    models: modified

--- books/models.py
+++ books/models.py
@@ -4,11 +4,10 @@ from django.db import models
 class Books(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     author = models.CharField(max_length=100, null=False, blank=False)
-    author = models.CharField(max_length=100, null=False, blank=False)
     gender = models.CharField(max_length=100, null=False, blank=False)
     pages = models.IntegerField(null=False, blank=False)
     copies = models.IntegerField(null=False, blank=False)
     isbn = models.IntegerField(null=False, blank=False)
     description = models.CharField(max_length=100, null=False, blank=False)
     publishing_company = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateField(auto_now_add=True)
