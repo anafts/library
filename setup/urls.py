3c0edcda495529e37e2f7cd53be1f0583a8f8c8d
Ana Freitas <ana.freitaas16@gmail.com>
2024-01-23 21:16:35 -0300

    created page router

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_list
+from books.views import BooksListView, BooksCreateView
 
 urlpatterns = [
     path("admin/", admin.site.urls), 
-    path("", books_list),
+    path("", BooksListView.as_view(), name="books_list"),
+    path("create", BooksCreateView.as_view(), name="books_create"),
 ]
